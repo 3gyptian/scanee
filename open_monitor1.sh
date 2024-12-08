c067172af24c9bf3d4625b61a62650490b539613
Your Name <you@example.com>
2024-12-08 00:18:26 -0800

    space bar toggles channel scan / AP scan

---
+++ open_monitor1.sh
@@ -0,0 +1,3 @@
+. /Users/nomad/esp/v5.2.1/esp-idf/export.sh                                                    
+python3 /Users/nomad/esp/v5.2.1/esp-idf/tools/idf_monitor.py -p /dev/cu.usbmodem21101 -b 115200
+
